798cf878f902fe523cc88683ad3cdfe0aa56cc64
yurecxgod <jurafko1337@gmail.com>
2021-07-26 11:09:53 +0300

    hw7

--- src/hw7/Bowl.java
+++ src/hw7/Bowl.java
@@ -1,5 +1,26 @@
 package hw7;
 
 public class Bowl {
+    private int food;
 
+    int getFood() {
+        return food;
+    }
+
+    Bowl(int food) {
+        this.food = food;
+    }
+
+    void decreaseFood(int n) {
+        food -= n;
+    }
+
+    void increaseFood() {
+        this.food += 300;
+        System.out.println("В миску добавили 300 грамм корма");
+    }
+
+    boolean checkFood(int n) {
+        return (food - n) >= 0;
+    }
 }
--- src/hw7/Cat.java
+++ src/hw7/Cat.java
@@ -1,5 +1,37 @@
 package hw7;
 
 public class Cat {
+    private String name;
+    private int appetite;
+    private int satietyTime;
+    private int satiety;
 
+    String getName() {
+        return name;
+    }
+
+    int getAppetite() {
+        return appetite;
+    }
+
+    int getSatiety() {
+        return satiety;
+    }
+
+    void setSatiety(int satiety) {
+        this.satiety = satiety;
+    }
+
+    Cat(String name, int appetite, int satietyTime) {
+
+        this.name = name;
+        this.appetite = appetite;
+        this.satietyTime = satietyTime;
+        this.satiety = 0;
+    }
+
+    void eat(Bowl p) {
+        p.decreaseFood(appetite);
+        satiety += satietyTime;
+    }
 }
