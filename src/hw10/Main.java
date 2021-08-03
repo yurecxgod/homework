42df6c015fd0bd2d6b6439e3f857b7a23f03eee0
yurecxgod <jurafko1337@gmail.com>
2021-08-04 01:12:54 +0300

    hw10

--- src/hw10/Main.java
+++ src/hw10/Main.java
@@ -1,7 +1,29 @@
 package hw10;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        String[] words = {"cold", "warm", "hot", "ice", "one", "two", "three", "four", "five", "six", "warm", "seven", "one"};
 
+        HashMap<String, Integer> wordsmap = new HashMap<>();
+        for (String s : words) {
+            wordsmap.put(s, wordsmap.getOrDefault(s, 0) + 1);
+        }
+        System.out.println(wordsmap + "\nВсего слов: " + wordsmap.size() + "\nУникальные слова: " + wordsmap.keySet());
+
+        Phones phoneDirectory = new Phones();
+        phoneDirectory.add("Невский", "+7921337228");
+        phoneDirectory.add("Майскй", "+7921337322");
+        phoneDirectory.add("Невский", "+7921337229");
+        phoneDirectory.add("Прикольный", "+7921337100");
+        phoneDirectory.add("Невский", "+7921337227");
+        phoneDirectory.add("Кентикславский", "+7921337300");
+
+
+        System.out.println("Номера по фамилии Невский: " + phoneDirectory.get("Невский"));
+        System.out.println("\nВесь телефонный справочник: " + phoneDirectory.PhoneDirectory.entrySet());
     }
 }
+
--- src/hw10/Phones.java
+++ src/hw10/Phones.java
@@ -1,4 +1,16 @@
 package hw10;
 
+import java.util.*;
+
 public class Phones {
+    Map<String, Set<String>> PhoneDirectory = new HashMap<>();
+
+    public void add(String name, String phoneNumber) {
+        Set<String> number = PhoneDirectory.getOrDefault(name, new HashSet<>());
+        number.add(phoneNumber);
+        PhoneDirectory.put(name, number);
+    }
+    Set<String> get(String name) {
+        return PhoneDirectory.get(name);
+    }
 }
