18b3c0ad2e7534df8bee5ffa75527528121dcbdf
yurecxgod <jurafko1337@gmail.com>
2021-08-09 17:50:37 +0300

    hw13

--- src/hw13/Main.java
+++ src/hw13/Main.java
@@ -5,26 +5,18 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    static final int CARS_COUNT = 4;
-    static final CountDownLatch countDownLatchFinish = new CountDownLatch(CARS_COUNT);
-    static final CountDownLatch countDownLatchReady = new CountDownLatch(CARS_COUNT);
-    static final CyclicBarrier startBarrier = new CyclicBarrier(CARS_COUNT);
-    public static final AtomicInteger finishCount = new AtomicInteger(0);
-
-
-    public static void main(String[] args) throws InterruptedException {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(80), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), countDownLatchFinish, finishCount);
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
-        for (Car car : cars) {
-            new Thread(car).start();
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
         }
-        countDownLatchReady.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        countDownLatchFinish.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
